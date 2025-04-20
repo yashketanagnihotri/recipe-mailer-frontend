4885904f50b704e933445af514b8bdfd919a0437
Yash Ketan Agnihotri <yashagni1992@gmail.com>
2025-04-20 05:39:36 +0530

    added ip

--- src/IngredientSelector.jsx
+++ src/IngredientSelector.jsx
@@ -60,7 +60,7 @@ const IngredientSelector = () => {
 
     try {
       const response = await axios.post(
-        "https://recipe-mailer-backend-production.up.railway.app/generate-recipes", // Use proxy URL here
+        "https://66.33.22.2/generate-recipes", // Use proxy URL here
         { ingredients: selectedIngredients },
         {
           headers: {
